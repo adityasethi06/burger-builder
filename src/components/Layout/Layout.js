83d9747e6565816599590e210c130f79b518f04d
adityasethi06 <u3524006@connect.hku.hk>
2018-07-07 01:18:13 +0800

    BurgerBuilder and BurgerIngredient components added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Aux from '../../hoc/Auxx';
+import classes from './Layout.css';
 
 const layout = (props) => (
+  <Aux>
     <div>toolbar, sideDrawer, backDrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
+  </Aux>
 );
+
+export default layout;
