013ec9342a5873c5692a7d8b8d046c5b50820b55
adityasethi06 <u3524006@connect.hku.hk>
2018-07-07 17:45:07 +0800

    BuildControls and BuildControl component added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,21 +1,56 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxx';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+      salad: 3,
+      cheese: 4,
+      meat: 8,
+      bacon: 9
+}
 
 class BurgerBuilder extends Component {
     state = {
       ingredients: {
         salad: 1,
         bacon: 1,
-        cheese: 1,
-        meat: 1
+        cheese: 0,
+        meat: 0
+      },
+      totalPrice: 20
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = {
+          ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+      if(this.state.ingredients[type] > 0){
+            const updatedCount = this.state.ingredients[type] - 1;
+            const updatedIngredients = {
+              ...this.state.ingredients
+            };
+            updatedIngredients[type] = updatedCount;
+            const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+            this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
       }
     }
+
+
     render() {
       return(
          <Aux>
             <Burger ingredients={this.state.ingredients}/>
-            <div>Build Controls</div>
+            <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}/>
          </Aux>
       );
     }
